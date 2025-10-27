8ac190236113d092496c39b7a5be075dd36fcc0f
Samuel <samueljonascali@gmail.com>
2025-10-27 19:37:51 -0300

    update app.py

--- wallet/apps.py
+++ wallet/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
 class WalletConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'wallet'
+
+    # ADICIONE ESTE MÉTODO:
+    def ready(self):
+        import wallet.signals  # Isso ativa os sinais
