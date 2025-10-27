5eb6f2c4814353302882539d887ae84e87a09b49
Samuel <samueljonascali@gmail.com>
2025-10-27 19:37:28 -0300

    modify admin.py

--- wallet/admin.py
+++ wallet/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Category, Account, Transaction
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'user')
+    list_filter = ('type', 'user')
+    search_fields = ('name',)
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'balance')
+    search_fields = ('name', 'user__username') # Corrigi 'user_username' para 'user__username'
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('date', 'user', 'account', 'type', 'amount', 'category')
+    list_filter = ('type', 'date', 'category')
+    search_fields = ('description',)
