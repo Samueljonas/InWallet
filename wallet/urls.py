f8c77f6e2557e712f9088005d8d74e0ad86a0ef2
Samuel <samueljonascali@gmail.com>
2025-10-28 13:52:39 -0300

    add urls of transactions list and delete

--- wallet/urls.py
+++ wallet/urls.py
@@ -4,12 +4,21 @@ from . import views
 app_name = 'wallet'
 
 urlpatterns = [
+    # Dashboard
     path('', views.DashboardView.as_view(), name='dashboard'),
     
-    # Caminho singular, como você definiu
-    path('transaction/', views.TransactionListView.as_view(), name='transactions_list'),
+    # --- CRUD de Transações ---
     
-    # Caminhos singulares
+    # Create (C)
     path('transaction/expense/new/', views.ExpenseCreateView.as_view(), name='expense_create'),
     path('transaction/income/new/', views.IncomeCreateView.as_view(), name='income_create'),
+    
+    # Read (R) - A Lista
+    path('transaction/', views.TransactionListView.as_view(), name='transaction_list'),
+    
+    # Update (U)
+    path('transaction/<int:pk>/edit/', views.TransactionUpdateView.as_view(), name='transaction_edit'),
+    
+    # Delete (D)
+    path('transaction/<int:pk>/delete/', views.TransactionDeleteView.as_view(), name='transaction_delete'),
 ]
