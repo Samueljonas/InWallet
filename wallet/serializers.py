1db227c2ebac49caef138a01b889e426362bcc3f
Samuel <samueljonascali@gmail.com>
2025-11-11 16:04:16 -0300

    update serializers.py

--- wallet/serializers.py
+++ wallet/serializers.py
@@ -1,12 +1,11 @@
-
 from rest_framework import serializers
-from .models import Transaction, Category
+# 1. Importe todos os três modelos
+from .models import Transaction, Account, Category 
 
 class TransactionSerializer(serializers.ModelSerializer):
     """
     Serializa o modelo Transaction para JSON e vice-versa.
     """
-    
     # Para deixar a API mais amigável, vamos incluir o nome
     # da categoria, não apenas o ID.
     category_name = serializers.CharField(source='category.name', read_only=True)
@@ -14,7 +13,7 @@ class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
         
-        # [cite_start]Define quais campos do seu modelo [cite: 1066-1076]
+        # Define quais campos do seu modelo
         # serão expostos na API.
         fields = [
             'id', 
@@ -29,5 +28,31 @@ class TransactionSerializer(serializers.ModelSerializer):
             'note',
             'user' # Incluímos o 'user' para referência
         ]
-
+        
+        # O 'user' não deve ser definido pelo cliente (React),
+        # e sim pela view (baseado na sessão), então é 'read_only'.
         read_only_fields = ['user']
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    """
+    Serializa o modelo Account.
+    O React usará isso para preencher os dropdowns de "Conta".
+    """
+    class Meta:
+        model = Account
+        fields = ['id', 'name', 'balance']
+        
+        # O saldo (balance) é 'read_only' pela API, pois é controlado
+        # pelos Signals. O React não deve poder mudá-lo diretamente.
+        read_only_fields = ['balance']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    """
+    Serializa o modelo Category.
+    O React usará isso para preencher os dropdowns de "Categoria".
+    """
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'type']
